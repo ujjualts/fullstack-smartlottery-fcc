081b53041952a012b2101f632f9e2b19bfa7339a
ujjual <ujjual72@gmail.com>
2022-07-24 23:50:31 +0530

    initialised raffle contract

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -4,5 +4,45 @@
 //winner to be selected every x minutes
 //chinlink oracle = random, automated execution(kepers)
 
-
 //SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+error Raffle_NotEnoughEthEntered();
+
+contract Raffle is VRFConsumerBaseV2 {
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    event RaffleEnter(address indexed player);
+
+    constructor(uint256 entranceFee, address vrfCoordinatorV2) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle_NotEnoughEthEntered();
+        }
+        s_players.push(payable(msg.sender));
+
+        emit RaffleEnter(msg.sender);
+    }
+
+    function requestRandomWinner() external {}
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
